e6648040f1b4bd32519f0677b3f11c70d48061f6
Igor Bezerra Leandro <igorigrleandro@gmail.com>
2022-03-29 19:49:34 -0300

    commit inicial

---
+++ play.js
@@ -0,0 +1,39 @@
+
+//Variáveis do carro
+let xCarro = 600;
+
+
+
+
+function setup() {
+  createCanvas(500, 400);
+}
+
+function draw() {
+  background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+}
+
+function mostraAtor(){
+  image(imagemDoAtor, xAtor, yAtor, 30, 30);
+}
+
+function mostraCarro(){
+  image(imagemCarro, xCarro, 40, 50, 40);
+}
+
+function movimentaCarro(){
+  xCarro -= 2;
+}
+
+function movimentaAtor(){
+  if(keyIsDown(UP_ARROW)){
+    yAtor -= 3;
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    yAtor += 3;
+  }
+}
