6cf9967af8cbb028c6962ceb32ada374e8e32b79
Igor Bezerra Leandro <igorigrleandro@gmail.com>
2022-03-31 17:07:41 -0300

    Abstração e melhoria de código

--- carro.js
+++ carro.js
@@ -5,23 +5,25 @@ let xCarros = [600, 600, 600];
 let velocidadeCarros = [2, 2.5, 3.2];
 
 function mostraCarro(){
-    for(let i = 0; i < imagemCarros.length; i = i + 1){
+    for(let i = 0; i < imagemCarros.length; i++){
         image(imagemCarros[i], xCarros[i], yCarros[i], 50, 40);
     }
 }
   
 function movimentaCarro(){
-    xCarros[0] -= velocidadeCarros[0];
-    xCarros[1] -= velocidadeCarros[1];
-    xCarros[2] -= velocidadeCarros[2];
+    for(let i = 0; i < xCarros.length; i++){
+        xCarros[i] -= velocidadeCarros[i];
+    }
 }
 
 function voltaPosicaoInicialDoCarro(){
-    if(xCarros[0] < -40){
-        xCarros[0] = 600;
-    }else if(xCarros[1] < -40){
-        xCarros[1] = 600;
-    }else if(xCarros[2] < -40){
-        xCarros[2] = 600;
+    for(let i = 0; i < xCarros.length; i++){
+        if(passouTodaATela(i)){
+            xCarros[i] = 600;
+        }
     }
+}
+
+function passouTodaATela(i){
+    return xCarros[i] < -50;
 }
